1a883cab85033a99cd76ec92d3fc6c01e479b2ae
25garcial <25garcial@camraiders.com>
2022-03-07 09:42:00 -0600

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let container=document.querySelector(.container);
-
+alert(1);
+var divCol=document.createElement("div");
+divCol.classList.add("r"+String(i));
+container.appendChild(divCol);
+alert(2);
 for (let i=0; i<=16; i++){
-    alert(1);
-    var divCol=document.createElement("div");
-    divCol.classList.add("r"+String(i));
-    container.appendChild(divCol);
-    alert(2);
+   
    }
