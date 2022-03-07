f6f09d1e013b9417deac3c79eaf1d5147e160f59
25garcial <25garcial@camraiders.com>
2022-03-07 09:40:32 -0600

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let container=document.querySelector(.container);
 
 for (let i=0; i<=16; i++){
+    alert(1);
     var divCol=document.createElement("div");
     divCol.classList.add("r"+String(i));
     container.appendChild(divCol);
