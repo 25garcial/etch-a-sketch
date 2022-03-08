509d2b49fc39fe7b7abc711c3cc42efe92e71832
25garcial <25garcial@camraiders.com>
2022-03-08 09:29:05 -0600

    .

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ for (let i=1; i<=16; i++){
     
     var divCol=document.createElement("div");
     divCol.classList.add(String(i));
-    divCol.textContent=("h"+String(i));
+    divCol.textContent=("x"+String(i));
     container.appendChild(divCol);
+
     for (let o=1; o<=16; o++){
-        var selector =document.querySelector("${i}")
+        
+        var selector =document.getElementByClassName(i);
+  
         var divCol=document.createElement("div");
+      
         divCol.classList.add("r"+String(o));
+        
         divCol.textContent=("h"+String(o));
+        
         selector.appendChild(divCol);
+        alert(1)
     }
    
    }
