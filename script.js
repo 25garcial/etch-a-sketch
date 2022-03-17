6c348232f30a1911b184dc88aee105dd2fdea48a
25garcial <91680716+25garcial@users.noreply.github.com>
2022-03-17 11:41:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,21 @@ for (let i=1; i<=16; i++){
         var divCol=document.createElement("div");
         divCol.classList.add("box");
         
-        divCol.textContent=("h"+String(o));
-        divCol.addEventListener("mouseover", function(e){e.target.style.backgroundColor = "red";})
+        //divCol.textContent=("h"+String(o));
+        divCol.addEventListener("mouseover", function(e){e.target.style.backgroundColor = "red";
+        e.target.style.borderColor = "red";})
 
         select.appendChild(divCol);
         
     }
    
    }
+ document.querySelector(".clear").addEventListener("click", function(){
+ var selector= document.getElementsByClassName("box");
+ for (var i=0; i<selector.length; i++){
+ selector[i].style.backgroundColor="blue";
+ selector[i].style.borderColor="black";
+ 
+ }
+ })
    
